9ec3321ad3e9674951e1aca2a43ee9ac2d4331f4
Mahmut Pinarbasi <mahmut.pinarbasi@mobisem.com>
2017-03-10 12:59:52 +0300

    + IGLDropDownMenuTypeNormal alpha set to 0 when folding.

--- IGLDropDownMenu/IGLDropDownMenu.m
+++ IGLDropDownMenu/IGLDropDownMenu.m
@@ -448,6 +448,10 @@
             item.layer.transform = CATransform3DPerspect(rotate, CGPointMake(0, 0), 200);
             break;
         }
+        case IGLDropDownMenuTypeNormal: {
+            item.alpha = 0.0;
+            break;
+        }
         default: {
             // reset rotate
             item.transform = CGAffineTransformMakeRotation(0);
